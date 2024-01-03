e56ba98ca74d98b44e388d0adbbcfe10401b8180
Jimmy Yakubu <jimiyaks3@gmail.com>
2024-01-03 11:14:46 -0700

    atlas db release

--- src/components/TodoRecords.jsx
+++ src/components/TodoRecords.jsx
@@ -3,16 +3,15 @@ import React from "react";
 import Todos from "@/model/Todos";
 
 export default async function TodoRecords() {
-  //const todos = await Todos.find();
+  const todos = await Todos.find();
 
-  //const count = await Todos.find().countDocuments();
+  const count = await Todos.find().countDocuments();
 
   return (
     <div className="container todoRecords">
       <div className="row g-4 text-center  align-items-center mt-4">
         <h1 className=" text-success">Todos Page</h1>
         <hr />
-        {/**
         <div className="container d-flex justify-content-center my-2">
           <div className="col-lg-6 col-8  p-1 rounded-3">
             <Link
@@ -45,7 +44,6 @@ export default async function TodoRecords() {
             </div>
           </div>
         ))}
-         */}
       </div>
     </div>
   );
